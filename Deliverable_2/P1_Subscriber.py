baa9e74254b87d6472c34cd37f8ede87a790ff76
Jason Xie (xix277) <xiejason2@gmail.com>
2024-01-23 17:57:53 -0600

    commit operation

---
+++ Deliverable_2/P1_Subscriber.py
@@ -0,0 +1,47 @@
+
+import random
+import time
+
+import paho.mqtt.client as mqtt
+
+
+
+broker = 'broker.hivemq.com'
+topic = "Try_this_on_for_size_xix277"
+client_id = "test_xix277"
+
+
+def connect_mqtt() -> mqtt:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker)
+    return client
+
+
+def subscribe(client: mqtt):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    try:
+        client = connect_mqtt()
+        subscribe(client)
+        client.loop_forever()
+    except KeyboardInterrupt:
+        client.loop_stop()
+        client.disconnect()
+        print("\nDisconnected")
+
+if __name__ == '__main__':
+    run()
