7dd3e4b606d91d23b9e8e90cf360d4aed69466b8
Jason Xie (xix277) <xiejason2@gmail.com>
2024-01-23 14:24:20 -0600

    added code for publisher.py

--- Deliverable_2/D2_Publisher.py
+++ Deliverable_2/D2_Publisher.py
@@ -1,2 +1,10 @@
 import paho.mqtt.client as mqtt
 
+# use public broker and declare objects/methods
+mqttBroker = "broker.hivemq.com" # public broker
+client = mqtt.Client("test_call_xix277") # client name
+client.connect(mqttBroker)
+
+# publish message
+client.publish("Test_msg_xix277", "Sup Dude :p") # topic is Test_msg_xix277
+
