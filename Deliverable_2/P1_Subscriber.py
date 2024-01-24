7c9af691978a416e6bdcffd92b004038c91b498a
Jason Xie (xix277) <xiejason2@gmail.com>
2024-01-23 19:52:30 -0600

    Added json library for message decoding

--- Deliverable_2/P1_Subscriber.py
+++ Deliverable_2/P1_Subscriber.py
@@ -1,14 +1,14 @@
 
 import random
 import time
-
+import json as json
 import paho.mqtt.client as mqtt
 
 #comment
 
 broker = 'broker.hivemq.com'
 topic = "Try_this_on_for_size_xix277"
-client_id = "test_xix277"
+client_id = "test_xix277_2"
 
 
 def connect_mqtt() -> mqtt:
@@ -27,7 +27,14 @@ def connect_mqtt() -> mqtt:
 
 def subscribe(client: mqtt):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        json_decode_msg = json.loads(msg.payload) # decode json message
+        # sum_data = sum(json_decode_msg)
+        print(json_decode_msg)
+        print(type(json_decode_msg))
+        # print(sum_data)
+        # print(msg.payload.decode())
+        # print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        # print(f"Message Content is of Type:", type(msg.payload.decode()))
 
     client.subscribe(topic)
     client.on_message = on_message
